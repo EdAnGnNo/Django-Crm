1b533ffda17e1a6dff3c43cfd6ddf3ee66d4d9da
Eddy Gonzalez <gonzalezeddyan@gmail.com>
2024-03-04 16:56:39 -0400

    main branches

---
+++ mydb.py
@@ -0,0 +1,13 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'developer@home'
+)
+
+#prepare a cursor object
+cursorObject = dataBase.cursor() 
+#create a database
+cursorObject.execute("CREATE DATABASE elderco")
+print('All Done')
